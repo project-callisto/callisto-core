e89230ccf2e766e37c217f974bc1782ca24ab0f4
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2015-08-20 18:30:42 -0700

    let exceptions bubble up during matching

--- delivery/matching.py
+++ delivery/matching.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-import bugsnag
 
 from .models import MatchReport, EmailNotification
 from .report_delivery import send_matching_report_to_school
@@ -7,26 +6,22 @@ from .report_delivery import send_matching_report_to_school
 def find_matches():
     new_identifiers = MatchReport.objects.filter(seen=False).order_by('identifier').distinct('identifier')
     for row in new_identifiers.values():
-        try:
-            matches = MatchReport.objects.filter(identifier=row['identifier'])
-            match_list = list(matches)
-            if len(match_list) > 1 :
-                seen_match_owners = [match.report.owner for match in match_list if match.seen]
-                new_match_owners = [match.report.owner for match in match_list if not match.seen]
-                all_owners = seen_match_owners + new_match_owners
-                # filter out reports made by the same person
-                if len(set(all_owners)) > 1:
-                    # only send notifications if new owners are actually new
-                    if not set(new_match_owners).issubset(set(seen_match_owners)):
-                        process_new_matches(match_list)
-                    #new owners all already have existing matches
-                    for match_report in match_list:
-                        match_report.report.match_found = True
-                        match_report.report.save()
-            matches.update(seen=True)
-        except Exception as e:
-            #TODO: real logging
-            bugsnag.notify(e)
+        matches = MatchReport.objects.filter(identifier=row['identifier'])
+        match_list = list(matches)
+        if len(match_list) > 1 :
+            seen_match_owners = [match.report.owner for match in match_list if match.seen]
+            new_match_owners = [match.report.owner for match in match_list if not match.seen]
+            all_owners = seen_match_owners + new_match_owners
+            # filter out reports made by the same person
+            if len(set(all_owners)) > 1:
+                # only send notifications if new owners are actually new
+                if not set(new_match_owners).issubset(set(seen_match_owners)):
+                    process_new_matches(match_list)
+                #new owners all already have existing matches
+                for match_report in match_list:
+                    match_report.report.match_found = True
+                    match_report.report.save()
+        matches.update(seen=True)
 
 def process_new_matches(matches):
     owners_notified = []
