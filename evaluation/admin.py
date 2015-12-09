c7d315a5101b65258169cc52a708551f7851256d
Kelsey <kelsey@sexualhealthinnovations.org>
2015-12-09 15:45:49 -0800

    restore choice editing in admin

--- evaluation/admin.py
+++ evaluation/admin.py
@@ -1,13 +1,5 @@
 from django.contrib import admin
-from reports.admin import RecordFormItemChildAdmin
-from reports.models import RecordFormItem
 from .models import EvalField
 
 class EvalFieldInline(admin.StackedInline):
     model = EvalField
-
-class ExtendedRecordFormAdmin(RecordFormItemChildAdmin):
-    inlines = (EvalFieldInline,)
-
-admin.site.unregister(RecordFormItem)
-admin.site.register(RecordFormItem, ExtendedRecordFormAdmin)
