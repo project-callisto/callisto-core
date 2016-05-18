5ac4ef8d4f46befbbba71ff7eec9885798ab5396
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2016-05-18 13:24:09 -0500

    test against multiple versions of Django

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Framework :: Django',
+        'Framework :: Django :: 1.7',
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
         'Intended Audience :: Developers',
