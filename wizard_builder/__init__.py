2db1ed3dfe554dce4cef3671632d08c73926e403
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2016-05-18 20:51:00 -0500

    add default app config

--- wizard_builder/__init__.py
+++ wizard_builder/__init__.py
@@ -1,1 +1,3 @@
 __version__ = '0.0.1'
+
+default_app_config = 'wizard_builder.apps.WizardBuilderConfig'
---
+++ wizard_builder/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WizardBuilderConfig(AppConfig):
+    name = 'wizard_builder'
+    verbose_name = "Wizard Builder"
