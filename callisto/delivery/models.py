9d50eda7e731b87203b8627e7dfe9c731bbe564e
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2016-06-21 15:31:34 -0700

    clean up TODOs

--- callisto/delivery/models.py
+++ callisto/delivery/models.py
@@ -113,7 +113,7 @@ class MatchReport(models.Model):
         return "Match report for report {0}".format(self.report.pk)
 
 class SentReport(PolymorphicModel):
-    #TODO: store link to s3 backup
+    # TODO: store link to s3 backup https://github.com/SexualHealthInnovations/callisto-core/issues/14
     sent = models.DateTimeField(auto_now_add=True)
     to_address = models.EmailField(blank=False, null=False, max_length=256)
 
