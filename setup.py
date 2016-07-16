61962c72eb0dddbf2ea908c8a860735d3120566d
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2016-07-16 16:12:52 -0700

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -9,17 +9,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-
-def get_version(*file_paths):
-    filename = os.path.join(os.path.dirname(__file__), *file_paths)
-    version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
-
-version = get_version('wizard_builder', '__init__.py')
+version = '0.0.2'
 
 if sys.argv[-1] == 'publish':
     try:
--- wizard_builder/__init__.py
+++ wizard_builder/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 default_app_config = 'wizard_builder.apps.WizardBuilderConfig'
