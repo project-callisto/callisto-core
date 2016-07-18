a9431d9dd75c14d9fe5c6499f8578073d35ea805
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2016-07-17 19:58:36 -0700

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '0.0.2'
+version = '0.0.3'
 
 if sys.argv[-1] == 'publish':
     try:
--- wizard_builder/__init__.py
+++ wizard_builder/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 default_app_config = 'wizard_builder.apps.WizardBuilderConfig'
