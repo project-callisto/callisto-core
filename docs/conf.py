1c458f9666e1da7fd67915e7d50ec574869d34a2
Trevor Alexander <trevor@weareredshift.com>
2016-07-19 15:04:35 -0700

    Added namespace to docs import

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 import os
 import sys
 
-import delivery
+from callisto import delivery
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
