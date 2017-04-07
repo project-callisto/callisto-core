78a879cf8950c6b6181b8132c0499620d2154a3d
Lynn Cyrin <firemagelynn@gmail.com>
2017-04-07 13:00:01 -0700

    delete, isntead of modify, content types

--- callisto/delivery/migrations/0010_email_notification_data_migration.py
+++ callisto/delivery/migrations/0010_email_notification_data_migration.py
@@ -5,22 +5,19 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 
-def update_content_types(source, target, apps, schema_editor):
+def update_content_types(source, apps, schema_editor):
     db_alias = schema_editor.connection.alias
     ContentType = apps.get_model('contenttypes', 'ContentType')
     content_type = ContentType.objects.using(db_alias).filter(
         app_label=source,
         model='emailnotification',
-    ).first()
-    if content_type:
-        content_type.app_label = target
-        content_type.save()
+    ).delete()
 
 def update_content_types_forward(apps, schema_editor):
-    update_content_types('delivery', 'notification', apps, schema_editor)
+    update_content_types('delivery', apps, schema_editor)
 
 def update_content_types_reverse(apps, schema_editor):
-    update_content_types('notification', 'delivery', apps, schema_editor)
+    update_content_types('notification', apps, schema_editor)
 
 class Migration(migrations.Migration):
 
