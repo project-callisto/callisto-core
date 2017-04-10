a89d25eb2dad7d3cae0c57a1db8866c21a600b02
Lynn Cyrin <firemagelynn@gmail.com>
2017-04-10 16:16:19 -0700

    fix site test

--- tests/test_sites.py
+++ tests/test_sites.py
@@ -1,7 +1,9 @@
+from django.test import TestCase
+
 from wizard_builder.models import QuestionPage
 
 
-class SitePageTest(StaticPagesTestCase):
+class SitePageTest(TestCase):
 
     def test_basic_created_question_page_comes_with_a_site(self):
         page = QuestionPage.objects.create()
