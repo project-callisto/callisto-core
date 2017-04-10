c2669aef062db7246deb2afd8597c28138aaae61
Lynn Cyrin <firemagelynn@gmail.com>
2017-04-10 14:25:47 -0700

    fix typos

--- callisto/notification/api.py
+++ callisto/notification/api.py
@@ -4,15 +4,15 @@ from django.conf import settings
 class EmailNotificationApi(object):
 
     @classmethod
-    def send_match_notification(match_report):
+    def send_match_notification(cls, match_report):
         """Notifies reporting user that a match has been found. Requires an EmailNotification called "match_notification."
-        Runs as a part of the match cli script, so it does not have access to a request context
 
         Args:
           match_report(MatchReport): MatchReport for which a match has been found
         """
-        notification = self.objects.get(name='match_notification')
+        notification = cls.objects.get(name='match_notification')
         from_email = '"Callisto Matching" <notification@{0}>'.format(settings.APP_URL)
         to = match_report.contact_email
         context = {'report': match_report.report}
         notification.send(to=[to], from_email=from_email, context=context)
+
