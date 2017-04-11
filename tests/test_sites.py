8889b5e85308d2c8b276e5b678de32cabe26516f
Lynn Cyrin <firemagelynn@gmail.com>
2017-04-11 13:36:16 -0700

    expand multisite tests

--- tests/test_sites.py
+++ tests/test_sites.py
@@ -1,5 +1,6 @@
 from wizard_builder.models import QuestionPage
 
+from django.contrib.sites.models import Site
 from django.test import TestCase
 
 
@@ -8,3 +9,17 @@ class SitePageTest(TestCase):
     def test_basic_created_question_page_comes_with_a_site(self):
         page = QuestionPage.objects.create()
         self.assertEqual(page.site.domain, 'example.com')
+
+    def test_on_site_increments_for_default_site(self):
+        count_before = QuestionPage.objects.on_site().count()
+        page = QuestionPage.objects.create()
+        count_after = QuestionPage.objects.on_site().count()
+        self.assertEqual(count_before + 1, count_after)
+
+    def test_on_site_does_not_increment_for_alternate_site(self):
+        count_before = QuestionPage.objects.on_site().count()
+        page = QuestionPage.objects.create()
+        page.site = Site.objects.create()
+        page.save()
+        count_after = QuestionPage.objects.on_site().count()
+        self.assertEqual(count_before, count_after)
