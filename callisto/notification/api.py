600eeed7b96dd7a351df2cdf7404789cfee19690
Lynn Cyrin <firemagelynn@gmail.com>
2017-04-12 15:41:20 -0700

    cleanup

--- callisto/notification/api.py
+++ callisto/notification/api.py
@@ -1,18 +1,22 @@
+from django.conf import settings
+
 from callisto.notification.models import EmailNotification
 
 
 class NotificationApi(object):
 
-    Model = EmailNotification
+    model = EmailNotification
+    from_email = '"Reports" <reports@{0}>'.format(settings.APP_URL)
 
-    def send_match_notification(user, match_report):
+    @classmethod
+    def send_match_notification(cls, user, match_report):
         """Notifies reporting user that a match has been found. Requires an EmailNotification called "match_notification."
 
         Args:
           user(User): reporting user
           match_report(MatchReport): MatchReport for which a match has been found
         """
-        notification = Model.objects.on_site().get(name='match_notification')
+        notification = cls.Model.objects.on_site().get(name='match_notification')
         from_email = '"Callisto Matching" <notification@{0}>'.format(settings.APP_URL)
         to = match_report.contact_email
         context = {'report': match_report.report}
