549871e926a134b5e7006b5994e51ec03b1e3261
Lynn Cyrin <firemagelynn@gmail.com>
2017-04-13 14:11:38 -0700

    re-order functions

--- callisto/notification/api.py
+++ callisto/notification/api.py
@@ -22,21 +22,6 @@ class NotificationApi(object):
     from_email = '"Reports" <reports@{0}>'.format(settings.APP_URL)
 
     @classmethod
-    def send_match_notification(cls, user, match_report):
-        """Notifies reporting user that a match has been found.
-        Requires an NotificationApi.model with `name="match_notification."`
-
-        Args:
-          user(User): reporting user
-          match_report(MatchReport): MatchReport for which a match has been found
-        """
-        notification = cls.model.objects.on_site().get(name='match_notification')
-        from_email = '"Callisto Matching" <notification@{0}>'.format(settings.APP_URL)
-        to = match_report.contact_email
-        context = {'report': match_report.report}
-        notification.send(to=[to], from_email=from_email, context=context)
-
-    @classmethod
     def send_report_to_school(cls, sent_full_report, decrypted_report):
         # TODO: https://github.com/SexualHealthInnovations/callisto-core/issues/150
         from callisto.delivery.report_delivery import PDFFullReport
@@ -64,6 +49,29 @@ class NotificationApi(object):
         cls.send_email_to_coordinator(pdf, 'match_delivery', report_id)
 
     @classmethod
+    def send_user_notification(cls, form, notification_name):
+        notification = cls.model.objects.on_site().get(name=notification_name)
+        preferred_email = form.cleaned_data.get('email')
+        to_email = preferred_email
+        from_email = '"Callisto Confirmation" <confirmation@{0}>'.format(settings.APP_URL)
+        notification.send(to=[to_email], from_email=from_email)
+
+    @classmethod
+    def send_match_notification(cls, user, match_report):
+        """Notifies reporting user that a match has been found.
+        Requires an NotificationApi.model with `name="match_notification."`
+
+        Args:
+          user(User): reporting user
+          match_report(MatchReport): MatchReport for which a match has been found
+        """
+        notification = cls.model.objects.on_site().get(name='match_notification')
+        from_email = '"Callisto Matching" <notification@{0}>'.format(settings.APP_URL)
+        to = match_report.contact_email
+        context = {'report': match_report.report}
+        notification.send(to=[to], from_email=from_email, context=context)
+
+    @classmethod
     def send_email_to_coordinator(cls, pdf_to_attach, notification_name, report_id):
         notification = cls.model.objects.on_site().get(name=notification_name)
 
@@ -85,11 +93,3 @@ class NotificationApi(object):
         email.attach(cls.report_filename.format(report_id), attachment.data, "application/octet-stream")
 
         email.send()
-
-    @classmethod
-    def send_user_notification(cls, form, notification_name):
-        notification = cls.model.objects.on_site().get(name=notification_name)
-        preferred_email = form.cleaned_data.get('email')
-        to_email = preferred_email
-        from_email = '"Callisto Confirmation" <confirmation@{0}>'.format(settings.APP_URL)
-        notification.send(to=[to_email], from_email=from_email)
