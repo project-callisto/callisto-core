8792f8f0c6c7da5c7892dee84c40236286c19ec8
Lynn Cyrin <firemagelynn@gmail.com>
2017-04-14 17:35:34 -0700

    add callisto.delivery.api.NotificationApi and tests for it

---
+++ callisto/delivery/api.py
@@ -0,0 +1,21 @@
+import importlib
+
+from django.conf import settings
+from django.utils.module_loading import import_string
+
+
+class NotificationApi(object):
+
+    def __init__(self):
+        self.set_api_class()
+
+    def __getattr__(self, attr):
+        return getattr(self.api, attr)
+
+    def set_api_class(self):
+        override_class_path = getattr(
+            settings,
+            'CALLISTO_NOTIFICATION_API',
+            'callisto.notification.api.NotificationApi',
+        )
+        self.api = import_string(override_class_path)
---
+++ tests/delivery/test_api.py
@@ -0,0 +1,25 @@
+from mock import patch
+
+from django.test import TestCase, override_settings
+
+from callisto.delivery.api import NotificationApi
+from tests.callistocore.forms import CustomNotificationApi
+
+class ApiTest(TestCase):
+
+    def setUp(self):
+        super(ApiTest, self).setUp()
+        self.mock_argument_1 = 'mock argument 1'
+        self.mock_argument_2 = 'mock argument 2'
+
+    @patch('callisto.notification.api.NotificationApi.send_report_to_school')
+    def test_default_api_call(self, mock_process):
+        NotificationApi().send_report_to_school(self.mock_argument_1, self.mock_argument_2)
+        mock_process.assert_called_once_with(self.mock_argument_1, self.mock_argument_2)
+
+    @override_settings(CALLISTO_NOTIFICATION_API='tests.callistocore.forms.CustomNotificationApi')
+    @patch('callisto.notification.api.NotificationApi.send_report_to_school')
+    def test_overriden_api_call(self, mock_process):
+        NotificationApi().send_report_to_school(self.mock_argument_1, self.mock_argument_2)
+        mock_process.assert_called_once_with(self.mock_argument_1, self.mock_argument_2)
+        self.assertEqual(NotificationApi().from_email, CustomNotificationApi.from_email)
