361f8d9b904fdfe58bb59307e616c3b191178989
Lynn Cyrin <firemagelynn@gmail.com>
2017-04-14 17:53:52 -0700

    remove resolved TODOs

--- callisto/delivery/management/commands/find_matches.py
+++ callisto/delivery/management/commands/find_matches.py
@@ -7,7 +7,5 @@ class Command(BaseCommand):
     help = 'finds matches and sends match reports'
 
     def handle(self, *args, **options):
-        # TODO: eventually: add test option that verifies that passed class can be imported & has necessary methods
-        # TODO (cont): https://github.com/SexualHealthInnovations/callisto-core/issues/56
         run_matching()
         self.stdout.write('Matching run')
