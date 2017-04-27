c1aa55537418676b54be2d69be2c36577fdc49ab
Lynn Cyrin <firemagelynn@gmail.com>
2017-04-27 15:36:50 -0700

    update tests

--- tests/notification/test_validation.py
+++ tests/notification/test_validation.py
@@ -1,17 +1,53 @@
+from django.conf import settings
 from django.core.exceptions import ValidationError
-from django.test import TestCase
+from django.contrib.sites.models import Site
+from django.test import TestCase, override_settings
 
 from callisto.notification.models import EmailNotification
 
 
 class EmailValidationTest(TestCase):
 
-    def test_duplicate_emails_not_allowed(self):
+    def setUp(self):
+        super(EmailValidationTest, self).setUp()
+        for i in range(1,10):
+            site, _ = Site.objects.get_or_create(
+                id=i,
+            )
+            site.domain = str(i)
+            site.save()
+
+    @override_settings()
+    def test_duplicate_emails_not_allowed_on_same_site(self):
+        del settings.SITE_ID
+        site_id = 1
         with self.assertRaises(ValidationError):
-            for i in range(2):
+            for i in range(10):
                 email = EmailNotification.objects.create(
-                    name='example email'
+                    name='example email',
+                    body='example email',
+                    subject='example email',
                 )
-                email.sites.add(1)
+                email.sites.add(site_id)
                 email.full_clean()
-        self.assertEqual(EmailNotification.objects.count(), 1)
+        self.assertEqual(EmailNotification.objects.on_site(site_id).count(), 1)
+
+    @override_settings()
+    def test_cannot_add_site_which_would_create_duplicate(self):
+        del settings.SITE_ID
+        for i in range(1,10):
+            email = EmailNotification.objects.create(
+                name='example email',
+                body='example email',
+                subject='example email',
+            )
+            email.sites.add(i)
+            email.full_clean()
+        with self.assertRaises(ValidationError):
+            email = EmailNotification.objects.get(
+                name='example email',
+                sites__id__in=[1],
+            )
+            email.sites.add(2)
+            email.full_clean()
+        self.assertEqual(EmailNotification.objects.on_site(2).count(), 1)
