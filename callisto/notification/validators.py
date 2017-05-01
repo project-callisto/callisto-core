c495d177a7ecc47f0cbd97799a0d75d1928a27f2
Lynn Cyrin <firemagelynn@gmail.com>
2017-05-01 13:41:27 -0700

    remove site instead of deleting email

--- callisto/notification/validators.py
+++ callisto/notification/validators.py
@@ -5,7 +5,7 @@ def validate_email_unique(email):
     EmailNotification = email.__class__
     for site in email.sites.all():
         if EmailNotification.objects.filter(name=email.name, sites__id__in=[site.id]).exclude(id=email.id):
-            email.delete()
+            email.sites.remove(site.id)
             raise ValidationError('''
-                    EmailNotification already exists with (name={} ,site__domain={})
+                    EmailNotification already existed with (name={} ,site__domain={})
                 '''.format(email.name, site.domain))
