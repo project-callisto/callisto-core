e7dc6142fd036bfc634e8de336d47dc511c908f8
Lynn Cyrin <firemagelynn@gmail.com>
2017-05-01 14:42:22 -0700

    remove all invalid sites instead of pre-emptively aborting with raise

--- callisto/notification/validators.py
+++ callisto/notification/validators.py
@@ -3,9 +3,12 @@ from django.core.exceptions import ValidationError
 
 def validate_email_unique(email):
     EmailNotification = email.__class__
+    invalid_sites = []
     for site in email.sites.all():
         if EmailNotification.objects.filter(name=email.name, sites__id__in=[site.id]).exclude(id=email.id):
+            invalid_sites.append(site.domain)
             email.sites.remove(site.id)
-            raise ValidationError('''
-                    EmailNotification already exists with (name={} ,site__domain={})
-                '''.format(email.name, site.domain))
+    if invalid_sites:
+        raise ValidationError('''
+                EmailNotification already exists with (name={} ,sites__domain__in=[{}])
+            '''.format(email.name, invalid_sites))
