6f3c6b4c7d107f55720482048894f2c1386bb748
Lynn Cyrin <firemagelynn@gmail.com>
2017-05-11 23:29:43 -0700

    multiplechoice choice test

--- tests/test_inheritence.py
+++ tests/test_inheritence.py
@@ -1,5 +1,6 @@
-from wizard_builder.models import QuestionPage, SingleLineText
-
+from wizard_builder.models import (
+    QuestionPage, SingleLineText, Choice, MultipleChoice
+)
 from django.test import TestCase
 
 
@@ -15,3 +16,9 @@ class InheritenceTest(TestCase):
         SingleLineText.objects.create(page=page)
         question = QuestionPage.objects.first().formquestion_set.first()
         self.assertTrue(isinstance(question.page, SingleLineText))
+
+    def test_choice_multiple_choice_instance(self):
+        page = QuestionPage.objects.create()
+        question = MultipleChoice.objects.create(page=page)
+        choice = Choice.objects.create(question=question)
+        self.assertTrue(isinstance(choice.question, MultipleChoice))
