8c0870b99c2bc1325bb93a737ed41022a30bfc79
Lynn Cyrin <firemagelynn@gmail.com>
2017-05-12 02:02:58 -0700

    slighly better assertion

--- tests/test_inheritence.py
+++ tests/test_inheritence.py
@@ -11,20 +11,20 @@ class InheritenceTest(TestCase):
         page = QuestionPage.objects.create()
         SingleLineText.objects.create(page_id=page.id)
         question = FormQuestion.objects.first()
-        self.assertTrue(isinstance(question.page, QuestionPage))
+        self.assertIsInstance(question.page, QuestionPage)
 
     def test_page_form_question_set_instance(self):
         page = QuestionPage.objects.create()
         SingleLineText.objects.create(page_id=page.id)
         question = PageBase.objects.first().formquestion_set.first()
-        self.assertTrue(isinstance(question, SingleLineText))
+        self.assertIsInstance(question, SingleLineText)
 
     def test_choice_multiple_choice_instance(self):
         page = QuestionPage.objects.create()
         question = MultipleChoice.objects.create(page_id=page.id)
         Choice.objects.create(question_id=question.id)
         choice = Choice.objects.first()
-        self.assertTrue(isinstance(choice.question, MultipleChoice))
+        self.assertIsInstance(choice.question, MultipleChoice)
 
     def test_conditional_instance(self):
         page = QuestionPage.objects.create()
@@ -32,5 +32,5 @@ class InheritenceTest(TestCase):
         Conditional.objects.create(question_id=question.id, page_id=page.id)
         condition = Conditional.objects.first()
         self.assertTrue(QuestionPage.objects.first().conditional)
-        self.assertTrue(isinstance(condition.question, MultipleChoice))
-        self.assertTrue(isinstance(condition.page, QuestionPage))
+        self.assertIsInstance(condition.question, MultipleChoice)
+        self.assertIsInstance(condition.page, QuestionPage)
