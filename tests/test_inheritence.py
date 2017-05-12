2a2e77c9b19967879efb5e42d0c1c9493e9050f0
Lynn Cyrin <firemagelynn@gmail.com>
2017-05-11 23:35:34 -0700

    more accurate tests

--- tests/test_inheritence.py
+++ tests/test_inheritence.py
@@ -1,6 +1,6 @@
 from wizard_builder.models import (
     QuestionPage, SingleLineText, Choice, MultipleChoice,
-    Conditional
+    Conditional, FormQuestion, PageBase
 )
 from django.test import TestCase
 
@@ -9,19 +9,21 @@ class InheritenceTest(TestCase):
 
     def test_question_page_instance(self):
         page = QuestionPage.objects.create()
-        question = SingleLineText.objects.create(page=page)
+        SingleLineText.objects.create(page_id=page.id)
+        question = FormQuestion.objects.first()
         self.assertTrue(isinstance(question.page, QuestionPage))
 
     def test_page_form_question_set_instance(self):
         page = QuestionPage.objects.create()
         SingleLineText.objects.create(page=page)
-        question = QuestionPage.objects.first().formquestion_set.first()
+        question = PageBase.objects.first().formquestion_set.first()
         self.assertTrue(isinstance(question.page, SingleLineText))
 
     def test_choice_multiple_choice_instance(self):
         page = QuestionPage.objects.create()
         question = MultipleChoice.objects.create(page=page)
-        choice = Choice.objects.create(question=question)
+        Choice.objects.create(question=question)
+        choice = FormQuestion.objects.first()
         self.assertTrue(isinstance(choice.question, MultipleChoice))
 
     def test_choice_multiple_choice_instance(self):
