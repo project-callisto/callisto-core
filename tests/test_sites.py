3032c1588d241ad36a3da75c70dee249843b4df7
Lynn Cyrin <firemagelynn@gmail.com>
2017-05-16 11:36:58 -0700

    update SITE_ID tests

--- tests/test_sites.py
+++ tests/test_sites.py
@@ -3,7 +3,7 @@ from wizard_builder.models import PageBase, QuestionPage, SingleLineText
 
 from django.conf import settings
 from django.contrib.sites.models import Site
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 
 class TempSiteID():
@@ -25,37 +25,40 @@ class TempSiteID():
 
 class SiteIDTest(TestCase):
 
-    def test_page_has_defaualt_site_attribute(self):
+    @override_settings()
+    def test_page_has_no_default_site_attribute(self):
         with TempSiteID(1):
             page = QuestionPage.objects.create()
-        self.assertEqual(page.site.domain, 'example.com')
+        self.assertEqual(page.site, None)
 
+    @override_settings()
     def test_on_site_respects_SITE_ID_setting(self):
         site_1_pages = 3
         site_2_pages = site_1_pages + 1
+        site_1 = Site.objects.get(id=1)
         site_2 = Site.objects.create()
-        with TempSiteID(1):
-            for i in range(site_1_pages):
-                QuestionPage.objects.create()
-            for i in range(site_2_pages):
-                with TempSiteID(site_2.id):
-                    QuestionPage.objects.create()
-            self.assertEqual(QuestionPage.objects.on_site().count(), site_1_pages)
 
+        for i in range(site_1_pages):
+            page = QuestionPage.objects.create(site_id=site_1.id)
+        for i in range(site_2_pages):
+            page = QuestionPage.objects.create(site_id=site_2.id)
+
+        with TempSiteID(site_1.id):
+            self.assertEqual(QuestionPage.objects.on_site().count(), site_1_pages)
         with TempSiteID(site_2.id):
             self.assertEqual(QuestionPage.objects.on_site().count(), site_2_pages)
 
-    def test_can_override_default_site_id(self):
+    @override_settings()
+    def test_can_override_site_id_when_setting_is_set(self):
         with TempSiteID(1):
-            page = QuestionPage.objects.create()
-            self.assertEqual(page.site.id, settings.SITE_ID)
+            site_1 = Site.objects.get(id=settings.SITE_ID)
+            site_2 = Site.objects.create()
+            page = QuestionPage.objects.create(site_id=site_2.id)
+            self.assertNotEqual(settings.SITE_ID, site_2.id)
+            self.assertNotEqual(page.site.id, settings.SITE_ID)
 
-        site_2 = Site.objects.create()
-        page.site = site_2
-        page.save()
         self.assertEqual(page.site_id, site_2.id)
         self.assertEqual(page.site.id, site_2.id)
-        self.assertNotEqual(settings.SITE_ID, site_2.id)
 
 
 class SiteRequestTest(TestCase):
