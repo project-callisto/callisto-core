3aab1c6e5a0da90195b989055afbc02c8573b2c5
Lynn Cyrin <firemagelynn@gmail.com>
2017-05-16 10:55:31 -0700

    fix import error

--- wizard_builder/admin/base.py
+++ wizard_builder/admin/base.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib import admin
 from django.core.urlresolvers import reverse_lazy
 from django.db import models
-from django.urls import reverse
+from django.core.urlresolvers import reverse
 from django.utils.html import format_html
 
 from ..models import Choice, FormQuestion
