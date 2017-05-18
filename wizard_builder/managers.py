91194759802f02031630935fc989b6e230986d77
Lynn Cyrin <firemagelynn@gmail.com>
2017-05-18 00:40:31 -0700

    more cleanup

--- wizard_builder/managers.py
+++ wizard_builder/managers.py
@@ -6,7 +6,20 @@ from django.conf import settings
 from django.contrib.sites.models import Site
 
 
-class DowncastingManagerMixin(object):
+class PageBaseQuerySet(InheritanceQuerySet):
+
+    def on_site(self, site_id=None):
+        site_id = site_id or Site.objects.get_current().id
+        return self.filter(
+            site__id=site_id,
+        )
+
+
+class FormQuestionQuerySet(InheritanceQuerySet):
+    pass
+
+
+class AutoDowncastingManager(InheritanceManager):
 
     def get_queryset(self):
         base_queryset = self._queryset(self.model, using=self._db)
@@ -19,25 +32,12 @@ class DowncastingManagerMixin(object):
             return base_queryset.select_subclasses()
 
 
-class PageBaseQuerySet(InheritanceQuerySet):
-
-    def on_site(self, site_id=None):
-        site_id = site_id or Site.objects.get_current().id
-        return self.filter(
-            site__id=site_id,
-        )
-
-
-class PageBaseManager(DowncastingManagerMixin, InheritanceManager):
+class PageBaseManager(AutoDowncastingManager):
     _queryset = PageBaseQuerySet
 
     def on_site(self, site_id=None):
         return self.get_queryset().on_site(site_id)
 
 
-class FormQuestionQuerySet(InheritanceQuerySet):
-    pass
-
-
-class FormQuestionManager(DowncastingManagerMixin, InheritanceManager):
+class FormQuestionManager(AutoDowncastingManager):
     _queryset = FormQuestionQuerySet
