8c218316eea861eec67a8ff4681b5778c2393884
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2017-05-26 18:07:29 -0700

    clean up matching_validators aka code climate shaming

--- callisto/delivery/matching_validators.py
+++ callisto/delivery/matching_validators.py
@@ -8,32 +8,42 @@ from django.forms import URLField
 logger = logging.getLogger(__name__)
 
 
-def clean_url(url):
+def _clean_url(url):
     url_field = URLField()
     return url_field.clean(url.strip())
 
 
+def _get_url_parts(url):
+    url = _clean_url(url)
+    return urlsplit(url)
+
+
+def _get_initial_path(url_parts):
+    return url_parts[2].strip('/').split('/')[0].lower()
+
+
+generic_twitter_urls = [
+        'i',
+        'following',
+        'followers',
+        'who_to_follow',
+        'settings',
+        'search',
+        'tos',
+        'privacy',
+        'about',
+    ]
+
+
 def twitter_validation_function(value):
-        generic_twitter_urls = [
-                'i',
-                'following',
-                'followers',
-                'who_to_follow',
-                'settings',
-                'search',
-                'tos',
-                'privacy',
-                'about',
-            ]
         path = None
         try:
-            url = clean_url(value)
-            url_parts = urlsplit(url)
+            url_parts = _get_url_parts(value)
             # check if acceptable domain
             domain = url_parts[1]
             if not (domain == 'twitter.com' or domain == 'www.twitter.com' or domain == 'mobile.twitter.com'):
                 return None
-            path = url_parts[2].strip('/').split('/')[0].lower()
+            path = _get_initial_path(url_parts)
         except ValidationError:
             if value.startswith('@'):
                 path = value[1:]
@@ -49,55 +59,57 @@ twitter_validation_info = {'validation': twitter_validation_function,
                            'example': 'https://twitter.com/twitter_handle or @twitter_handle',
                            'unique_prefix': 'twitter'}
 
+
+generic_fb_urls = [
+    'messages',
+    'hashtag',
+    'events',
+    'pages',
+    'groups',
+    'bookmarks',
+    'lists',
+    'developers',
+    'topic',
+    'help',
+    'privacy',
+    'campaign',
+    'policies',
+    'support',
+    'settings',
+    'games',
+    'people',]
+
+
+def facebook_validation_function(url):
+        try:
+            url_parts = _get_url_parts(url)
+            # check if acceptable domain
+            domain = url_parts[1]
+            if not (domain == 'facebook.com' or domain.endswith('.facebook.com')):
+                return None
+            path = _get_initial_path(url_parts)
+
+            # old style numeric profiles
+            if path == "profile.php":  # ex. https://www.facebook.com/profile.php?id=100010279981469
+                path = parse_qs(url_parts[3]).get('id')[0]
+            if path == 'people':  # ex. https://www.facebook.com/people/John-Doe/100013326345115
+                path = url_parts[2].strip('/').split('/')[2].lower()
+
+            # TODO: validate against allowed username characteristics
+            # https://github.com/SexualHealthInnovations/callisto-core/issues/181
+            if not path or path == "" or path.endswith('.php') or path in generic_fb_urls:
+                return None
+            else:
+                return path
+        except ValidationError:
+            return None
+
+
 '''
  NOTE: because identifiers are irreversibly encrypted and Facebook was the original matching identifier, Facebook
  identifiers are stored plain, with the prefix "www.facebook.com/" stripped. All other identifiers should be prefixed
  to allow for global uniqueness from Facebook profile identifiers.
 '''
-
-def facebook_validation_function(url):
-        try:
-            url = clean_url(url)
-        except ValidationError:
-            return None
-        url_parts = urlsplit(url)
-        # check if acceptable domain
-        domain = url_parts[1]
-        if not (domain == 'facebook.com' or domain.endswith('.facebook.com')):
-            return None
-        path = url_parts[2].strip('/').split('/')[0].lower()
-
-        # old style numeric profiles
-        if path == "profile.php":  # ex. https://www.facebook.com/profile.php?id=100010279981469
-            path = parse_qs(url_parts[3]).get('id')[0]
-        if path == 'people': # ex. https://www.facebook.com/people/John-Doe/100013326345115
-            path = url_parts[2].strip('/').split('/')[2].lower()
-
-        generic_fb_urls = [
-            'messages',
-            'hashtag',
-            'events',
-            'pages',
-            'groups',
-            'bookmarks',
-            'lists',
-            'developers',
-            'topic',
-            'help',
-            'privacy',
-            'campaign',
-            'policies',
-            'support',
-            'settings',
-            'games',
-            'people']
-        # TODO: validate against allowed username characteristics
-        # https://github.com/SexualHealthInnovations/callisto-core/issues/181
-        if not path or path == "" or path.endswith('.php') or path in generic_fb_urls:
-            return None
-        else:
-            return path
-
 facebook_validation_info = {'validation': facebook_validation_function,
                             'example': 'http://www.facebook.com/johnsmithfakename',
                             'unique_prefix': ''}
