7ea061eb5a7627dad708ee1b655795cc57fec29b
Lynn Cyrin <firemagelynn@gmail.com>
2017-07-19 16:29:18 -0700

    cleanup

--- callisto_core/evaluation/management/commands/decrypt_eval_data.py
+++ callisto_core/evaluation/management/commands/decrypt_eval_data.py
@@ -18,8 +18,7 @@ class Command(BaseCommand):
     help = 'decrypts eval data. can only be run in local environments (import data from prod)'
 
     def handle(self, *args, **options):
-        eval_key = settings.CALLISTO_EVAL_PRIVATE_KEY
-        if not eval_key:
+        if not settings.CALLISTO_EVAL_PRIVATE_KEY:
             raise ImproperlyConfigured('CALLISTO_EVAL_PRIVATE_KEY not present')
         decrypted_eval_data = []
         for row in EvalRow.objects.all():
@@ -29,7 +28,7 @@ class Command(BaseCommand):
                              'action': row.action,
                              'timestamp': row.timestamp.timestamp()}
             gpg = gnupg.GPG()
-            gpg.import_keys(eval_key)
+            gpg.import_keys(settings.CALLISTO_EVAL_PRIVATE_KEY)
             decrypted_eval_row = six.text_type(gpg.decrypt(six.binary_type(row.row)))
             if decrypted_eval_row:
                 decrypted_row.update(json.loads(decrypted_eval_row))
