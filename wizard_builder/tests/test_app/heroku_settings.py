f52f9c92f791ac166517e1437fe557ef24e33cca
Lynn Cyrin <firemagelynn@gmail.com>
2017-08-03 14:31:16 -0700

    heroku settings

---
+++ wizard_builder/tests/test_app/heroku_settings.py
@@ -0,0 +1,7 @@
+import dj_database_url
+
+from wizard_builder.tests.test_app.settings import *
+
+DATABASES = {
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL')),
+}
--- wizard_builder/tests/test_app/wsgi.py
+++ wizard_builder/tests/test_app/wsgi.py
@@ -1,2 +1,4 @@
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "heroku_settings"
