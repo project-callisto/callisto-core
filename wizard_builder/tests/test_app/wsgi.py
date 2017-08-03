14a4b39b7515f01121586130b915dc8ba22381f6
Lynn Cyrin <firemagelynn@gmail.com>
2017-08-03 14:39:25 -0700

    fix wsgi.py

--- wizard_builder/tests/test_app/wsgi.py
+++ wizard_builder/tests/test_app/wsgi.py
@@ -1,4 +1,5 @@
+import os
+os.environ["DJANGO_SETTINGS_MODULE"] = "wizard_builder.tests.test_app.heroku_settings"
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-os.environ["DJANGO_SETTINGS_MODULE"] = "heroku_settings"
