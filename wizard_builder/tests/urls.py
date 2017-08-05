1d480f824988825ad9e57a28ac1a706dee5f2552
Lynn Cyrin <firemagelynn@gmail.com>
2017-08-05 14:52:48 -0700

    add a set of urls for the homepage

--- wizard_builder/tests/urls.py
+++ wizard_builder/tests/urls.py
@@ -1,11 +1,33 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse_lazy
 
 from .test_app.views import edit_test_wizard_view, new_test_wizard_view
 
 urlpatterns = [
-    url(r'^wizard/new/(?P<step>.+)/$', new_test_wizard_view, name="test_wizard"),
-    url(r'^wizard/edit/(?P<edit_id>\d+)/(?P<step>.+)/$', edit_test_wizard_view, name='test_edit_wizard'),
+    url(r'^$',
+        RedirectView.as_view(
+            url=reverse_lazy('new_test_wizard'),
+            permanent=True,
+        ),
+        name='index',
+    ),
+    url(r'^wizard/new/$',
+        RedirectView.as_view(
+            url=reverse_lazy('test_wizard', kwargs={'step': 0}),
+            permanent=True,
+        ),
+        name='new_test_wizard',
+    ),
+    url(r'^wizard/new/(?P<step>.+)/$',
+        new_test_wizard_view,
+        name='test_wizard',
+    ),
+    url(r'^wizard/edit/(?P<edit_id>\d+)/(?P<step>.+)/$',
+        edit_test_wizard_view,
+        name='test_edit_wizard',
+    ),
     url(r'^admin/', admin.site.urls),
     url(r'^tinymce/', include('tinymce.urls')),
 ]
