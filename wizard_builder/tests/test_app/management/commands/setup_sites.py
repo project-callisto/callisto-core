1ca6ba2b5498858aa4d706c8257fb2aa22938e85
Lynn Cyrin <firemagelynn@gmail.com>
2017-08-07 02:31:58 -0700

    fix setup_sites for heroky deploy

--- wizard_builder/tests/test_app/management/commands/setup_sites.py
+++ wizard_builder/tests/test_app/management/commands/setup_sites.py
@@ -10,22 +10,19 @@ User = get_user_model()
 
 class Command(BaseCommand):
 
-    def _setup_allowed_hosts(self):
-        if getattr(settings, 'ALLOWED_HOSTS', None):
-            for host in settings.ALLOWED_HOSTS:
-                Site.objects.get_or_create(domain=host)
-
     def _setup_current_site(self):
         if getattr(settings, 'HEROKU_APP_NAME', None):
             current_domain = settings.HEROKU_APP_NAME
         else:
             current_domain = settings.APP_URL
-        Site.objects.filter(
-            id=1,
-        ).update(
-            domain=current_domain,
-        )
+        Site.objects.exclude(id=1).delete()
+        Site.objects.filter(id=1).update(domain=current_domain)
+
+    def _setup_allowed_hosts(self):
+        if getattr(settings, 'ALLOWED_HOSTS', None):
+            for host in settings.ALLOWED_HOSTS:
+                Site.objects.get_or_create(domain=host)
 
     def handle(self, *args, **options):
-        self._setup_allowed_hosts()
         self._setup_current_site()
+        self._setup_allowed_hosts()
