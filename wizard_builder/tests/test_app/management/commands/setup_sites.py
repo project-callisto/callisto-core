451867052da1fc4b00cfed7443eb7518d34df4ee
Lynn Cyrin <firemagelynn@gmail.com>
2017-08-07 01:30:23 -0700

    setup_sites for heroku apps

--- wizard_builder/tests/test_app/management/commands/setup_sites.py
+++ wizard_builder/tests/test_app/management/commands/setup_sites.py
@@ -1,5 +1,7 @@
 import os
 
+from django.conf import settings
+from django.contrib.sites.models import Site
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
@@ -8,14 +10,22 @@ User = get_user_model()
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **options):
-        from django.conf import settings
-        from django.contrib.sites.models import Site
-        if getattr(settings, 'ALLOWED_HOSTS'):
+    def _setup_allowed_hosts(self):
+        if getattr(settings, 'ALLOWED_HOSTS', None):
             for host in settings.ALLOWED_HOSTS:
                 Site.objects.get_or_create(domain=host)
+
+    def _setup_current_site(self):
+        if getattr(settings, 'HEROKU_APP_NAME', None):
+            current_domain = settings.HEROKU_APP_NAME
         else:
-            Site.objects.exclude(id=1).delete()
-            site = Site.objects.get(id=1)
-            site.domain = settings.APP_URL
-            site.save()
+            current_domain = settings.APP_URL
+        Site.objects.filter(
+            id=1,
+        ).update(
+            domain=current_domain,
+        )
+
+    def handle(self, *args, **options):
+        self._setup_allowed_hosts()
+        self._setup_current_site()
