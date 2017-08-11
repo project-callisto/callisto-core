23c494492c201b8664faa0adfc3f39159fcb8ab4
Lynn Cyrin <firemagelynn@gmail.com>
2017-08-11 11:17:24 -0700

    generalize security implementation a bit

--- callisto_core/delivery/models.py
+++ callisto_core/delivery/models.py
@@ -1,7 +1,5 @@
 import uuid
 
-import nacl.secret
-import nacl.utils
 import six
 from nacl.exceptions import CryptoError
 from polymorphic.models import PolymorphicModel
@@ -12,82 +10,7 @@ from django.utils import timezone
 from django.utils.crypto import get_random_string
 
 from .hashers import get_hasher, make_key
-
-
-def _encrypt_report(stretched_key, report_text):
-    """Encrypts a report using the given secret key & salt. Requires a stretched key with a length of 32 bytes.
-    The encryption uses PyNacl & Salsa20 stream cipher.
-
-    Args:
-      salt (str): cryptographic salt
-      stretched_key (str): secret key after being stretched
-      report_text (str): full report as a string
-
-    Returns:
-      bytes: the encrypted bytes of the report
-
-    """
-    box = nacl.secret.SecretBox(stretched_key)
-    message = report_text.encode('utf-8')
-    nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
-    return box.encrypt(message, nonce)
-
-
-def _decrypt_report(stretched_key, encrypted):
-    """Decrypts an encrypted report.
-
-    Args:
-      salt (str): cryptographic salt
-      stretched_key (str): secret key after being stretched
-      encrypted (bytes): full report encrypted
-
-    Returns:
-      str: the decrypted report as a string
-
-    Raises:
-      CryptoError: If the key and salt fail to decrypt the record.
-
-    """
-    box = nacl.secret.SecretBox(stretched_key)
-    decrypted = box.decrypt(bytes(encrypted))  # need to force to bytes bc BinaryField can return as memoryview
-    return decrypted.decode('utf-8')
-
-
-def _pepper(encrypted_report):
-    """Uses a secret value stored on the server to encrypt an already encrypted report, to add protection if the database
-    is breached but the server is not. Requires settings.PEPPER to be set to a 32 byte value. In production, this value
-    should be set via environment parameter. Uses PyNacl's Salsa20 stream cipher.
-
-    Args:
-      encrypted_report (bytes): the encrypted report
-
-    Returns:
-      bytes: a further encrypted report
-
-    """
-    pepper = settings.PEPPER
-    box = nacl.secret.SecretBox(pepper)
-    nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
-    return box.encrypt(encrypted_report, nonce)
-
-
-def _unpepper(peppered_report):
-    """Decrypts a report that has been peppered with the _pepper method. Requires settings.PEPPER to be set to a 32
-    byte value. In production, this value should be set via environment parameter.
-
-    Args:
-      peppered_report(bytes): a report that has been encrypted using a secret key then encrypted using the pepper
-
-    Returns:
-      bytes: the report, still encrypted with the secret key
-
-    Raises:
-      CryptoError: If the pepper fails to decrypt the record.
-    """
-    pepper = settings.PEPPER
-    box = nacl.secret.SecretBox(pepper)
-    decrypted = box.decrypt(bytes(peppered_report))  # need to force to bytes bc BinaryField can return as memoryview
-    return decrypted
+from . import security
 
 
 class Report(models.Model):
@@ -134,13 +57,12 @@ class Report(models.Model):
           report_text (str): the full text of the report
           secret_key (str): the secret key
         """
+        if self.salt:
+            self.salt = None
         hasher = get_hasher()
         encoded = hasher.encode(secret_key, get_random_string())
         self.encode_prefix, stretched_key = hasher.split_encoded(encoded)
-        self.encrypted = _encrypt_report(
-            stretched_key=stretched_key,
-            report_text=report_text,
-        )
+        self.encrypted = security.encrypt_text(stretched_key, report_text)
         self.save()
 
     def decrypted_report(self, key):
@@ -155,8 +77,7 @@ class Report(models.Model):
           CryptoError: If the key and saved salt fail to decrypt the record.
         """
         prefix, stretched_key = make_key(self.encode_prefix, key, self.salt)
-
-        return _decrypt_report(stretched_key=stretched_key, encrypted=self.encrypted)
+        return security.decrypt_text(stretched_key, self.encrypted)
 
     def withdraw_from_matching(self):
         """ Deletes all associated MatchReports """
@@ -215,7 +136,7 @@ class MatchReport(models.Model):
         encoded = hasher.encode(key, salt)
         self.encode_prefix, stretched_key = hasher.split_encoded(encoded)
 
-        self.encrypted = _pepper(_encrypt_report(stretched_key=stretched_key, report_text=report_text))
+        self.encrypted = security.pepper(security.encrypt_text(stretched_key=stretched_key, report_text=report_text))
 
     def get_match(self, identifier):
         """Checks if the given identifier triggers a match on this report. Returns report text if so.
