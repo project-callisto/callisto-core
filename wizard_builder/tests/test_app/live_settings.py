ed3219e9b4b37309a206448ab1554c486c44054b
Lynn Cyrin <firemagelynn@gmail.com>
2017-08-12 14:18:18 -0700

    isolate live settings database

--- wizard_builder/tests/test_app/live_settings.py
+++ wizard_builder/tests/test_app/live_settings.py
@@ -1,5 +1,11 @@
 from wizard_builder.tests.test_app.app_settings import *
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+    },
+}
+
 DEBUG = False
 MIDDLEWARE_CLASSES = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
