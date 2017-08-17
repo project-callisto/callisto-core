0aa65c610198ad0bcccc0b51b26a7a76dcdf471f
Lynn Cyrin <firemagelynn@gmail.com>
2017-08-17 14:33:57 -0700

    move tests, remove outdated tests

--- tests/delivery/test_views.py
+++ tests/delivery/test_views.py
@@ -122,3 +122,308 @@ class NewReportFlowTest(ReportFlowHelper):
 
         self.assertFalse(getattr(form, 'decrypted_report', False))
         self.assertIsInstance(form, ReportAccessForm)
+
+
+class ReportMetaFlowTest(ReportFlowHelper):
+
+    def test_deletes_report(self):
+        self.assertEqual(Report.objects.count(), 1)
+        response = self.client.post(
+            (self.delete_url % self.report.id),
+            data={'key': self.report_key},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context.get('report_deleted'))
+        self.assertEqual(Report.objects.count(), 0)
+
+    def test_export_returns_pdf(self):
+        response = self.client.post(
+            (self.export_url % self.report.id),
+            data={'key': self.report_key},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEquals(
+            response.get('Content-Disposition'),
+            'attachment; filename="report.pdf"'
+        )
+
+    def test_export_pdf_has_report(self):
+        response = self.client.post(
+            (self.export_url % self.report.id),
+            data={'key': self.report_key},
+        )
+        self.assertEqual(response.status_code, 200)
+        exported_report = BytesIO(response.content)
+        pdf_reader = PyPDF2.PdfFileReader(exported_report)
+        self.assertNotIn("Tatiana Nine", pdf_reader.getPage(0).extractText())
+        self.assertIn("Reported by: testing_12", pdf_reader.getPage(0).extractText())
+        self.assertIn("test answer", pdf_reader.getPage(1).extractText())
+        self.assertIn("another answer to a different question", pdf_reader.getPage(1).extractText())
+
+    def test_match_report_is_withdrawn(self):
+        self.assertEqual(MatchReport.objects.filter(report=self.report).count(), 1)
+        response = self.client.get(self.withdrawal_url % self.report.pk)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(MatchReport.objects.filter(report=self.report).count(), 0)
+
+
+    def test_submit_creates_match(self):
+        response = self.client.post((self.submission_url % self.report.pk),
+                                    data={'name': 'test submitter',
+                                          'email': 'test@example.com',
+                                          'phone_number': '555-555-1212',
+                                          'email_confirmation': "False",
+                                          'key': self.report_key,
+                                          'form-0-perp': 'facebook.com/test_url',
+                                          'form-TOTAL_FORMS': '1',
+                                          'form-INITIAL_FORMS': '1',
+                                          'form-MAX_NUM_FORMS': '', })
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn('submit_error', response.context)
+        self.assertEqual(self.report.id, MatchReport.objects.latest('id').report.id)
+
+    def test_multiple_perps_creates_multiple_matches(self):
+        total_matches_before = MatchReport.objects.count()
+        response = self.client.post((self.submission_url % self.report.pk),
+                                    data={'name': 'test submitter',
+                                          'email': 'test@example.com',
+                                          'phone_number': '555-555-1212',
+                                          'email_confirmation': "False",
+                                          'key': self.report_key,
+                                          'form-0-perp': 'facebook.com/test_url1',
+                                          'form-1-perp': 'facebook.com/test_url2',
+                                          'form-TOTAL_FORMS': '2',
+                                          'form-INITIAL_FORMS': '1',
+                                          'form-MAX_NUM_FORMS': '', })
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn('submit_error', response.context)
+        total_matches_after = MatchReport.objects.count()
+        self.assertEqual(total_matches_after - total_matches_before, 2)
+
+    @override_settings(CALLISTO_NOTIFICATION_API='tests.callistocore.forms.SiteAwareNotificationApi')
+    def test_match_sends_report_immediately(self):
+        self.client.post((self.submission_url % self.report.pk),
+                         data={'name': 'test submitter 1',
+                               'email': 'test1@example.com',
+                               'phone_number': '555-555-1212',
+                               'email_confirmation': "False",
+                               'key': self.report_key,
+                               'form-0-perp': 'facebook.com/triggered_match',
+                               'form-TOTAL_FORMS': '1',
+                               'form-INITIAL_FORMS': '1',
+                               'form-MAX_NUM_FORMS': '', })
+        user2 = User.objects.create_user(username='testing_122', password='testing_12')
+        self.client.login(username='testing_122', password='testing_12')
+        report2_text = """[
+    { "answer": "test answer",
+      "id": %i,
+      "section": 1,
+      "question_text": "first question",
+      "type": "SingleLineText"
+    },
+    { "answer": "another answer to a different question",
+      "id": %i,
+      "section": 1,
+      "question_text": "2nd question",
+      "type": "SingleLineText"
+    }
+  ]""" % (self.question1.pk, self.question2.pk)
+        report2 = Report(owner=user2)
+        report2_key = 'a key a key a key a key key'
+        report2.encrypt_report(report2_text, report2_key)
+        report2.save()
+        response = self.client.post((self.submission_url % report2.pk),
+                                    data={'name': 'test submitter 2',
+                                          'email': 'test2@example.com',
+                                          'phone_number': '555-555-1213',
+                                          'email_confirmation': "False",
+                                          'key': report2_key,
+                                          'form-0-perp': 'facebook.com/triggered_match',
+                                          'form-TOTAL_FORMS': '1',
+                                          'form-INITIAL_FORMS': '1',
+                                          'form-MAX_NUM_FORMS': '', })
+        self.assertNotIn('submit_error', response.context)
+        self.assertEqual(len(mail.outbox), 3)
+        message = mail.outbox[0]
+        self.assertEqual(message.subject, 'test match notification')
+        self.assertEqual(message.to, ['test1@example.com'])
+        self.assertIn('Matching" <notification@', message.from_email)
+        self.assertIn('test match notification body', message.body)
+        message = mail.outbox[1]
+        self.assertEqual(message.subject, 'test match notification')
+        self.assertEqual(message.to, ['test2@example.com'])
+        self.assertIn('Matching" <notification@', message.from_email)
+        self.assertIn('test match notification body', message.body)
+        message = mail.outbox[2]
+        self.assertEqual(message.subject, 'test match delivery')
+        self.assertEqual(message.to, ['titleix@example.com'])
+        self.assertIn('"Reports" <reports@', message.from_email)
+        self.assertIn('test match delivery body', message.body)
+        self.assertRegexpMatches(message.attachments[0][0], 'report_.*\\.pdf\\.gpg')
+
+    @override_settings(CALLISTO_NOTIFICATION_API='tests.callistocore.forms.SiteAwareNotificationApi')
+    @override_settings(CALLISTO_IDENTIFIER_DOMAINS=validators.facebook_or_twitter)
+    def test_non_fb_match(self):
+        self.client.post((self.submission_url % self.report.pk),
+                         data={'name': 'test submitter 1',
+                               'email': 'test1@example.com',
+                               'phone_number': '555-555-1212',
+                               'email_confirmation': "False",
+                               'key': self.report_key,
+                               'form-0-perp': 'twitter.com/trigger_a_match',
+                               'form-TOTAL_FORMS': '1',
+                               'form-INITIAL_FORMS': '1',
+                               'form-MAX_NUM_FORMS': '', })
+        user2 = User.objects.create_user(username='testing_122', password='testing_12')
+        self.client.login(username='testing_122', password='testing_12')
+        report2_text = """[
+    { "answer": "test answer",
+      "id": %i,
+      "section": 1,
+      "question_text": "first question",
+      "type": "SingleLineText"
+    },
+    { "answer": "another answer to a different question",
+      "id": %i,
+      "section": 1,
+      "question_text": "2nd question",
+      "type": "SingleLineText"
+    }
+  ]""" % (self.question1.pk, self.question2.pk)
+        report2 = Report(owner=user2)
+        report2_key = 'a key a key a key a key key'
+        report2.encrypt_report(report2_text, report2_key)
+        report2.save()
+        response = self.client.post((self.submission_url % report2.pk),
+                                    data={'name': 'test submitter 2',
+                                          'email': 'test2@example.com',
+                                          'phone_number': '555-555-1213',
+                                          'email_confirmation': "False",
+                                          'key': report2_key,
+                                          'form-0-perp': 'twitter.com/Trigger_A_Match',
+                                          'form-TOTAL_FORMS': '1',
+                                          'form-INITIAL_FORMS': '1',
+                                          'form-MAX_NUM_FORMS': '', })
+        self.assertNotIn('submit_error', response.context)
+        self.assertEqual(len(mail.outbox), 3)
+        message = mail.outbox[2]
+        self.assertEqual(message.subject, 'test match delivery')
+        self.assertEqual(message.to, ['titleix@example.com'])
+        self.assertIn('"Reports" <reports@', message.from_email)
+        self.assertIn('test match delivery body', message.body)
+        self.assertRegexpMatches(message.attachments[0][0], 'report_.*\\.pdf\\.gpg')
+
+    @override_settings(MATCH_IMMEDIATELY=False)
+    @override_settings(CALLISTO_NOTIFICATION_API='tests.callistocore.forms.SiteAwareNotificationApi')
+    def test_match_sends_report_delayed(self):
+        self.client.post((self.submission_url % self.report.pk),
+                         data={'name': 'test submitter 1',
+                               'email': 'test1@example.com',
+                               'phone_number': '555-555-1212',
+                               'email_confirmation': "False",
+                               'key': self.report_key,
+                               'form-0-perp': 'facebook.com/triggered_match',
+                               'form-TOTAL_FORMS': '1',
+                               'form-INITIAL_FORMS': '1',
+                               'form-MAX_NUM_FORMS': '', })
+        user2 = User.objects.create_user(username='testing_122', password='testing_12')
+        self.client.login(username='testing_122', password='testing_12')
+        report2_text = """[
+    { "answer": "test answer",
+      "id": %i,
+      "section": 1,
+      "question_text": "first question",
+      "type": "SingleLineText"
+    },
+    { "answer": "another answer to a different question",
+      "id": %i,
+      "section": 1,
+      "question_text": "2nd question",
+      "type": "SingleLineText"
+    }
+  ]""" % (self.question1.pk, self.question2.pk)
+        report2 = Report(owner=user2)
+        report2_key = 'a key a key a key a key key'
+        report2.encrypt_report(report2_text, report2_key)
+        report2.save()
+        response = self.client.post((self.submission_url % report2.pk),
+                                    data={'name': 'test submitter 2',
+                                          'email': 'test2@example.com',
+                                          'phone_number': '555-555-1213',
+                                          'email_confirmation': "False",
+                                          'key': report2_key,
+                                          'form-0-perp': 'facebook.com/triggered_match',
+                                          'form-TOTAL_FORMS': '1',
+                                          'form-INITIAL_FORMS': '1',
+                                          'form-MAX_NUM_FORMS': '', })
+        self.assertNotIn('submit_error', response.context)
+        self.assertEqual(len(mail.outbox), 0)
+        call_command('find_matches')
+        self.assertEqual(len(mail.outbox), 3)
+        message = mail.outbox[0]
+        self.assertEqual(message.subject, 'test match notification')
+        self.assertEqual(message.to, ['test1@example.com'])
+        self.assertIn('Matching" <notification@', message.from_email)
+        self.assertIn('test match notification body', message.body)
+        message = mail.outbox[1]
+        self.assertEqual(message.subject, 'test match notification')
+        self.assertEqual(message.to, ['test2@example.com'])
+        self.assertIn('Matching" <notification@', message.from_email)
+        self.assertIn('test match notification body', message.body)
+        message = mail.outbox[2]
+        self.assertEqual(message.subject, 'test match delivery')
+        self.assertEqual(message.to, ['titleix@example.com'])
+        self.assertIn('"Reports" <reports@', message.from_email)
+        self.assertIn('test match delivery body', message.body)
+        self.assertRegexpMatches(message.attachments[0][0], 'report_.*\\.pdf\\.gpg')
+
+    @override_settings(CALLISTO_NOTIFICATION_API='tests.callistocore.forms.CustomNotificationApi')
+    def test_match_sends_custom_report(self):
+        self.client.post(('/test_reports/match_custom/%s/' % self.report.pk),
+                         data={'name': 'test submitter 1',
+                               'email': 'test1@example.com',
+                               'phone_number': '555-555-1212',
+                               'email_confirmation': "False",
+                               'key': self.report_key,
+                               'form-0-perp': 'facebook.com/triggered_match',
+                               'form-TOTAL_FORMS': '1',
+                               'form-INITIAL_FORMS': '1',
+                               'form-MAX_NUM_FORMS': '', })
+        user2 = User.objects.create_user(username='testing_122', password='testing_12')
+        self.client.login(username='testing_122', password='testing_12')
+        report2_text = """[
+    { "answer": "test answer",
+      "id": %i,
+      "section": 1,
+      "question_text": "first question",
+      "type": "SingleLineText"
+    },
+    { "answer": "another answer to a different question",
+      "id": %i,
+      "section": 1,
+      "question_text": "2nd question",
+      "type": "SingleLineText"
+    }
+  ]""" % (self.question1.pk, self.question2.pk)
+        report2 = Report(owner=user2)
+        report2_key = 'a key a key a key a key key'
+        report2.encrypt_report(report2_text, report2_key)
+        report2.save()
+        response = self.client.post(('/test_reports/match_custom/%s/' % report2.pk),
+                                    data={'name': 'test submitter 2',
+                                          'email': 'test2@example.com',
+                                          'phone_number': '555-555-1213',
+                                          'email_confirmation': "False",
+                                          'key': report2_key,
+                                          'form-0-perp': 'facebook.com/triggered_match',
+                                          'form-TOTAL_FORMS': '1',
+                                          'form-INITIAL_FORMS': '1',
+                                          'form-MAX_NUM_FORMS': '', })
+        self.assertNotIn('submit_error', response.context)
+        self.assertEqual(len(mail.outbox), 3)
+        message = mail.outbox[2]
+        self.assertEqual(message.subject, 'test match delivery')
+        self.assertEqual(message.to, ['titleix@example.com'])
+        self.assertIn('"Custom" <custom@', message.from_email)
+        self.assertIn('test match delivery body', message.body)
+        self.assertRegexpMatches(message.attachments[0][0], 'custom_.*\\.pdf\\.gpg')
