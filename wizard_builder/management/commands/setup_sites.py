63c5b5460bcdde2dee1ed729f6e6447eb6a94246
Lynn Cyrin <firemagelynn@gmail.com>
2017-08-19 16:53:58 -0700

    update site setup code

--- wizard_builder/management/commands/setup_sites.py
+++ wizard_builder/management/commands/setup_sites.py
@@ -6,18 +6,16 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def _setup_current_site(self):
-        if getattr(settings, 'HEROKU_REVIEW_APP_DOMAIN', None):
-            current_domain = settings.HEROKU_REVIEW_APP_DOMAIN
+        if os.getenv('HEROKU_APP_NAME', default=''):
+            review_app_domain = os.getenv('HEROKU_APP_NAME') + '.herokuapp.com'
+        else:
+            review_app_domain = ''
+        if review_app_domain:
+            current_domain = settings.review_app_domain
         else:
             current_domain = settings.APP_URL
-        Site.objects.exclude(id=1).delete()
+        Site.objects.filter(domain=current_domain).exclude(id=1).delete()
         Site.objects.filter(id=1).update(domain=current_domain)
-
-    def _setup_allowed_hosts(self):
-        if getattr(settings, 'ALLOWED_HOSTS', None):
-            for host in settings.ALLOWED_HOSTS:
-                Site.objects.get_or_create(domain=host)
 
     def handle(self, *args, **options):
         self._setup_current_site()
-        self._setup_allowed_hosts()
--- wizard_builder/tests/test_app/heroku_settings.py
+++ wizard_builder/tests/test_app/heroku_settings.py
@@ -5,12 +5,7 @@ DATABASES = {
     'default': dj_database_url.parse(os.getenv('DATABASE_URL')),
 }
 
-if os.getenv('HEROKU_APP_NAME', default=''):
-    HEROKU_REVIEW_APP_DOMAIN = os.getenv('HEROKU_APP_NAME') + '.herokuapp.com'
-else:
-    HEROKU_REVIEW_APP_DOMAIN = ''
-
 ALLOWED_HOSTS = [
     APP_URL,
-    HEROKU_REVIEW_APP_DOMAIN,
+    '.herokuapp.com',
 ]
