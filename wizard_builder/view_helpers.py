420df2db2f6449b7de698d63f3a0ce3dc65f1e3a
Lynn Cyrin <firemagelynn@gmail.com>
2017-09-18 20:48:50 -0700

    logging, test, potential fix for review screen issue?

--- wizard_builder/view_helpers.py
+++ wizard_builder/view_helpers.py
@@ -58,7 +58,10 @@ class SerializedDataHelper(object):
             self._parse_answers(question, answer)
 
     def _parse_answers(self, question, answer):
-        if isinstance(answer, str):
+        print()
+        print('question: ', question)
+        print('answer: ', answer)
+        if question.get('type') == 'Singlelinetext':
             self._append_text_answer(answer, question)
         else:
             self._append_list_answers(answer, question)
@@ -70,6 +73,8 @@ class SerializedDataHelper(object):
         self._append_answer(question, [answer])
 
     def _append_list_answers(self, answer, question):
+        if isinstance(answer, str):
+            answer = [answer]
         choice_list = [
             self._get_choice_text(answer, question)
         ]
