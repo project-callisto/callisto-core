8e3f67a4e148ce5649b72f45aae72f8048955b49
Lynn Cyrin <firemagelynn@gmail.com>
2017-10-17 18:10:28 -0700

    TDD for form persistence

--- wizard_builder/tests/test_managers.py
+++ wizard_builder/tests/test_managers.py
@@ -9,6 +9,16 @@ class ManagerTest(TestCase):
         'wizard_builder_data',
     ]
 
+    def _change_form_text(self, form):
+        question = models.FormQuestion.objects.filter(pk=form['id'])
+        question.update(text='text should be persistent')
+
+    def _serialize(self, forms):
+        return [
+            form.serialized
+            for form in forms
+        ]
+
     def test_accurate_number_of_forms_present(self):
         self.assertEqual(
             len(self.manager.get_form_models()),
@@ -24,3 +34,14 @@ class ManagerTest(TestCase):
         data = {'question_2': text}
         form = self.manager.get_form_models(answer_data=data)[1]
         self.assertEqual(form.cleaned_data, data)
+
+    def test_manager_persists_form_data(self):
+        form_data_before = self.manager.get_serialized_forms()
+        self._change_form_text(form_data_before[0][0])
+        form_data_after = self._serialize(self.manager.get_form_models(
+            form_data=form_data_before))
+        self.assertNotEqual(
+            'text should be persistent',
+            form_data_after[0][0]['question_text'],
+        )
+        self.assertEqual(form_data_before, form_data_after)
