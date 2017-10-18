c5c393528c00a49d64b4e40ffc8b04759fd89c51
Lynn Cyrin <firemagelynn@gmail.com>
2017-10-17 21:19:21 -0700

    function name update

--- wizard_builder/tests/test_data_helper.py
+++ wizard_builder/tests/test_data_helper.py
@@ -13,7 +13,7 @@ class DataHelperTest(TestCase):
     ]
 
     def test_data_generates(self):
-        form = self.manager.get_forms()[1]
+        form = self.manager.get_form_models()[1]
         zipped_data = self.data_manager.get_zipped_data(
             data={'question_2': 'owlette ipsum cattree'},
             forms=[form.serialized],
@@ -21,7 +21,7 @@ class DataHelperTest(TestCase):
         self.assertIsInstance(zipped_data, list)
 
     def test_data_textbox(self):
-        form = self.manager.get_forms()[1]
+        form = self.manager.get_form_models()[1]
         zipped_data = self.data_manager.get_zipped_data(
             data={'question_2': 'owlette ipsum cattree'},
             forms=[form.serialized],
@@ -34,7 +34,7 @@ class DataHelperTest(TestCase):
         )
 
     def test_data_multiple_choice(self):
-        form = self.manager.get_forms()[0]
+        form = self.manager.get_form_models()[0]
         zipped_data = self.data_manager.get_zipped_data(
             data={'question_1': ['3']},
             forms=[form.serialized],
@@ -45,14 +45,14 @@ class DataHelperTest(TestCase):
         )
 
     def test_data_non_does_not_raise_error(self):
-        form = self.manager.get_forms()[0]
+        form = self.manager.get_form_models()[0]
         zipped_data = self.data_manager.get_zipped_data(
             data={'question_1': None},
             forms=[form.serialized],
         )
 
     def test_data_multiple_choice_multiple_options(self):
-        form = self.manager.get_forms()[0]
+        form = self.manager.get_form_models()[0]
         zipped_data = self.data_manager.get_zipped_data(
             data={'question_1': ['1', '2']},
             forms=[form.serialized],
@@ -63,7 +63,7 @@ class DataHelperTest(TestCase):
         )
 
     def test_data_multiple_choice_string(self):
-        form = self.manager.get_forms()[0]
+        form = self.manager.get_form_models()[0]
         zipped_data = self.data_manager.get_zipped_data(
             data={'question_1': '3'},
             forms=[form.serialized],
@@ -75,7 +75,7 @@ class DataHelperTest(TestCase):
 
     @skip('WIP')
     def test_data_multiple_choice_extra_info_empty(self):
-        form = self.manager.get_forms()[0]
+        form = self.manager.get_form_models()[0]
         zipped_data = self.data_manager.get_zipped_data(
             data={
                 'question_1': ['1'],
@@ -89,7 +89,7 @@ class DataHelperTest(TestCase):
 
     @skip('WIP')
     def test_data_multiple_choice_extra_info_filled(self):
-        form = self.manager.get_forms()[0]
+        form = self.manager.get_form_models()[0]
         zipped_data = self.data_manager.get_zipped_data(
             data={
                 'question_1': ['1'],
