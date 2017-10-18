476d6fcf5ae74e62848384cdff0d9ed09cf4a136
Lynn Cyrin <firemagelynn@gmail.com>
2017-10-17 18:03:35 -0700

    update manager tests

--- wizard_builder/tests/test_managers.py
+++ wizard_builder/tests/test_managers.py
@@ -11,16 +11,16 @@ class ManagerTest(TestCase):
 
     def test_accurate_number_of_forms_present(self):
         self.assertEqual(
-            len(self.manager.get_forms({}, 1)),
+            len(self.manager.get_form_models()),
             len(models.Page.objects.wizard_set(1)),
         )
 
     def test_returns_page_form(self):
-        for form in self.manager.get_forms():
+        for form in self.manager.get_form_models():
             self.assertIsInstance(form, forms.PageForm)
 
     def test_manager_populates_default_data(self):
         text = 'kitten ipsum cottoncloud'
         data = {'question_2': text}
-        form = self.manager.get_forms(data)[1]
+        form = self.manager.get_form_models(answer_data=data)[1]
         self.assertEqual(form.cleaned_data, data)
