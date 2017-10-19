5f63e424fd4a575b8746929b5c923e87b9e05d26
requires.io <support@requires.io>
2017-10-18 21:43:50 -0400

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'django',
         'django-polymorphic>=1.0',
         'django-ratelimit>=1.0',
-        'django-wizard-builder>=2.1,<2.2',
+        'django-wizard-builder>=2.4,<2.5',
         'PyNaCl>=1.0',
         'python-gnupg>=0.4',
         'pytz>=2017',
