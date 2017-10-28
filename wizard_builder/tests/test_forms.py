d7909d3f213b3fa213fc8f89c0bc16203ba24e0b
Lynn Cyrin <firemagelynn@gmail.com>
2017-10-28 02:17:36 -0700

    fill out expected data

--- wizard_builder/tests/test_forms.py
+++ wizard_builder/tests/test_forms.py
@@ -12,7 +12,15 @@ class FormSerializationTest(TestCase):
     def test_serialized_data_format(self):
         form = self.manager.get_form_models()[1]
         data = form.serialized
-        expected_data = {}
-        from pprint import pprint
-        pprint(data)
+        expected_data = [{
+            'descriptive_text': 'answer wisely',
+            'field_id': 'question_2',
+            'formquestion_ptr': 2,
+            'id': 2,
+            'page': 2,
+            'position': 0,
+            'question_text': 'do androids dream of electric sheep?',
+            'text': 'do androids dream of electric sheep?',
+            'type': 'Singlelinetext',
+        }]
         self.assertEqual(data, expected_data)
