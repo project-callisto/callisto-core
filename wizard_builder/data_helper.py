04b72dd14a90f732027dbc972d4cf5b871f47849
Lynn Cyrin <firemagelynn@gmail.com>
2017-10-28 04:43:27 -0700

    show list answers if the question has choices

--- wizard_builder/data_helper.py
+++ wizard_builder/data_helper.py
@@ -50,7 +50,7 @@ class SerializedDataHelper(object):
             self._parse_answers(question, answer)
 
     def _parse_answers(self, question, answer):
-        if question.get('type').lower() in ['checkbox', 'radiobutton']:
+        if question.get('choices'):
             self._append_list_answers(answer, question)
         else:
             self._append_text_answer(answer, question)
