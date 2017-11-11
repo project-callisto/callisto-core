94185242ba575efadee38e7c7d815d0e398585a9
Lynn Cyrin <firemagelynn@gmail.com>
2017-11-10 21:26:45 -0800

    cleanup example implementation

--- callisto_core/utils/tenant_api.py
+++ callisto_core/utils/tenant_api.py
@@ -1,13 +1,11 @@
 from distutils.util import strtobool
+from django.contrib.sites.shortcuts import get_current_site
 
 
 def cast_string_to_type(
     value: str,
     cast: [str, bool, int]
 ) -> [str, bool, int]:
-    '''
-    reference implementation, not actually used in this repo
-    '''
     if cast is str:
         return value
     elif cast is bool:
@@ -21,36 +19,28 @@ def cast_string_to_type(
 class CallistoCoreTenantApi(object):
 
     @staticmethod
-    def site_settings(var, cast=str, request=None, site_id=None):
+    def site_settings(var, cast=str, request=None, site_id=1):
         '''
-        api is locked to a similar function in a callisto campus private repo
-
         Implement this function to provide multi-tenant functionality for
         you application. You need to be able to derive your current tenant from
         either request or site_id (preferably both)
 
-        an example implementation could look like:
-
-        return {
-            1: {
-                'COORDINATOR_NAME': 'cat',
-                'COORDINATOR_EMAIL': 'cat@example.com',
-            },
-            2: {
-                'COORDINATOR_NAME': 'dog',
-                'COORDINATOR_EMAIL': 'dog@example.com',
-            },
-        }[site_id][var]
-
-        (callisto campus's implementation is a bit more complex than the above)
-
-        takes a `cast` param to cast to that type
-            cast params can be:
-                str (the default)
-                bool
-                int
+        The implementation here is a naive mock of a similar implementation in
+        callisto campus
         '''
-        return {
-            'COORDINATOR_NAME': 'COORDINATOR_NAME',
-            'COORDINATOR_EMAIL': 'COORDINATOR_EMAIL@example.com',
-        }[var]
+        if request:
+            try:
+                site_id = get_current_site(self.request).id
+            except BaseException:
+                site_id = 1
+
+        site_values = {
+            1: {
+                'COORDINATOR_NAME': 'COORDINATOR_NAME',
+                'COORDINATOR_EMAIL': 'COORDINATOR_EMAIL@example.com',
+            }
+        }.get(site_id, {})
+        value = site_values.get(var, '')
+        value = cast_string_to_type(value, cast)
+
+        return value
