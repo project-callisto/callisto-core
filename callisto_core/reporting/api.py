da10edbfc01b1f8b1655849400caec6f01553b5d
Lynn Cyrin <firemagelynn@gmail.com>
2017-11-19 19:33:00 -0800

    logging

--- callisto_core/reporting/api.py
+++ callisto_core/reporting/api.py
@@ -27,7 +27,7 @@ class CallistoCoreMatchingApi(object):
         for func in self.transforms:
             match_list = func(match_list)
         if match_list:
-            logger.info("new match found")
+            logger.info(f"new matches({len(match_list)}) found")
         return match_list
 
     def _resolve_duplicate_owners(self, match_list):
@@ -38,11 +38,14 @@ class CallistoCoreMatchingApi(object):
             if match.report.owner not in report_owners:
                 new_match_list.append(match)
                 report_owners.append(match.report.owner)
+            else:
+                logger.debug("duplicate report owner")
 
         return new_match_list
 
     def _resolve_single_report(self, match_list):
         if len(match_list) == 1:
+            logger.debug("no valid matches found")
             return []
         else:
             return match_list
