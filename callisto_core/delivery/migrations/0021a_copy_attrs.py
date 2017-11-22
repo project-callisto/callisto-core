b788e69ce89750aa5149ca0f2792c58da84eab83
Lynn Cyrin <firemagelynn@gmail.com>
2017-11-22 12:40:02 -0800

    attr names

--- callisto_core/delivery/migrations/0021a_copy_attrs.py
+++ callisto_core/delivery/migrations/0021a_copy_attrs.py
@@ -7,9 +7,9 @@ from django.db import migrations
 
 def _copy_attrs_for_instances(instances):
     for instance in instances:
-        page.new_sent = page.sent
-        page.new_to_address = page.to_address
-        page.save()
+        instance.new_sent = instance.sent
+        instance.new_to_address = instance.to_address
+        instance.save()
 
 
 def copy_attrs(apps, schema_editor):
