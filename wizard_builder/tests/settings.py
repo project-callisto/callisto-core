a7527717dae121315377c6afc64ce8f68faa6679
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2017-11-22 05:18:04 +0000

    Added site middleware

--- wizard_builder/tests/settings.py
+++ wizard_builder/tests/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
 ]
 
 TEMPLATES = [
