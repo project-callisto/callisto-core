10a81221833ac0d491e0c856061cc2558ca3e0ab
Lynn Cyrin <firemagelynn@gmail.com>
2017-11-23 17:22:43 -0800

    WIP test setup

--- callisto_core/tests/delivery/test_frontend.py
+++ callisto_core/tests/delivery/test_frontend.py
@@ -1,22 +1,29 @@
+from urllib.parse import urlparse
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from django.contrib.auth import get_user_model
+from django.contrib.sites.models import Site
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import override_settings
 from django.urls import reverse
 
+from callisto_core.delivery.models import Report
 from wizard_builder.tests import test_frontend as wizard_builder_tests
 
 User = get_user_model()
 
 
-class PassphraseMixin:
+class AuthMixin:
     passphrase = 'soooooo seekrit'
+    username = 'demo'
+    password = 'demo'
 
 
 class ElementHelper(
     wizard_builder_tests.ElementHelper,
-    PassphraseMixin,
+    AuthMixin,
 ):
 
     def enter_key(self):
@@ -31,21 +38,42 @@ class ElementHelper(
         self.browser.find_element_by_css_selector(
             '[type="submit"]').click()
 
+    def enter_username(self):
+        self.browser.find_element_by_css_selector(
+            '[name="username"]').send_keys(self.password)
 
+    def enter_password(self):
+        self.browser.find_element_by_css_selector(
+            '[name="password"]').send_keys(self.username)
+
+
+@override_settings(
+    DEBUG=True,
+    SITE_ID=1,
+)
 class FrontendTestCase(
     wizard_builder_tests.FrontendTest,
-    PassphraseMixin,
+    AuthMixin,
 ):
-    username = 'demo'
-    password = 'demo'
+    fixtures = [
+        'wizard_builder_data',
+        'callisto_core_notification_data',
+    ]
 
     @property
-    def element(self):
-        return ElementHelper(self.browser)
+    def element(cls):
+        return ElementHelper(cls.browser)
 
     @classmethod
     def setUpClass(cls):
         super(StaticLiveServerTestCase, cls).setUpClass()
+        cls.setup_browser()
+        cls.setup_site()
+        cls.setup_user()
+        cls.create_report()
+
+    @classmethod
+    def setup_browser(cls):
         chrome_options = Options()
         # chrome_options.add_argument("--headless")
         # chrome_options.add_argument('--no-sandbox')
@@ -53,37 +81,45 @@ class FrontendTestCase(
         cls.browser = webdriver.Chrome(
             chrome_options=chrome_options,
         )
+
+    @classmethod
+    def setup_user(cls):
         User.objects.create_user(
             username=cls.username,
             password=cls.password,
         )
+        cls.browser.get(cls.live_server_url)
+        ElementHelper(cls.browser).enter_username()
+        ElementHelper(cls.browser).enter_password()
+        ElementHelper(cls.browser).submit()
+
+    @classmethod
+    def create_report(cls):
+        cls.browser.get(cls.live_server_url + reverse('report_new'))
+        ElementHelper(cls.browser).enter_key()
+        ElementHelper(cls.browser).enter_key_confirmation()
+        ElementHelper(cls.browser).submit()
+        cls.report = Report.objects.first()
+
+    @classmethod
+    def setup_site(cls):
+        port = urlparse(cls.live_server_url).port
+        Site.objects.filter(id=1).update(domain='localhost:{}'.format(port))
+        cls.site = Site.objects.filter(id=1)
 
     def setUp(self):
         super().setUp()
-        self._setup_user()
-        self._create_report()
-
-    def _setup_user(self):
-        self.client.login(
-            username=self.username,
-            password=self.password,
+        url = reverse(
+            'report_update',
+            kwargs={'uuid': self.report.uuid, 'step': 0}
         )
-        cookie = self.client.cookies['sessionid']
-        self.browser.get(self.live_server_url)
-        self.browser.add_cookie({
-            'name': 'sessionid',
-            'value': cookie.value,
-            'secure': False,
-            'path': '/',
-        })
-
-    def _create_report(self):
-        self.browser.get(self.live_server_url + reverse('report_new'))
-        self.element.enter_key()
-        self.element.enter_key_confirmation()
-        self.element.submit()
+        self.browser.get(self.live_server_url + url)
 
 
+@override_settings(
+    DEBUG=True,
+    SITE_ID=1,
+)
 class EncryptedFrontendTest(
     FrontendTestCase,
 ):
