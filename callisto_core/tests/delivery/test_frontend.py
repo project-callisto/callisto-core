55c6d7617269f099ffc403219f6eb3a96703ea50
Lynn Cyrin <firemagelynn@gmail.com>
2017-11-23 16:34:28 -0800

    add in logins

--- callisto_core/tests/delivery/test_frontend.py
+++ callisto_core/tests/delivery/test_frontend.py
@@ -5,26 +5,76 @@ from wizard_builder.tests import test_frontend as wizard_builder_tests
 User = get_user_model()
 
 
-class CoreElementHelper(
-    wizard_builder_tests.ElementHelper,
-):
-    pass
-
-
-class EncryptedFrontendTest(
-    wizard_builder_tests.FrontendTest,
-):
+class PassphraseMixin:
     passphrase = 'soooooo seekrit'
+
+
+class ElementHelper(
+    wizard_builder_tests.ElementHelper,
+    PassphraseMixin,
+):
+
+    def enter_key(self):
+        self.browser.find_element_by_css_selector(
+            '[name="key"]').send_keys(self.passphrase)
+
+    def enter_key_confirmation(self):
+        self.browser.find_element_by_css_selector(
+            '[name="key_confirmation"]').send_keys(self.passphrase)
+
+    def submit(self):
+        self.browser.find_element_by_css_selector(
+            '[type="submit"]').click()
+
+
+class FrontendTestCase(
+    wizard_builder_tests.FrontendTest,
+    PassphraseMixin,
+):
+    username = 'demo'
+    password = 'demo'
 
     @property
     def element(self):
-        return CoreElementHelper(self.browser)
+        return ElementHelper(self.browser)
 
     def setUp(self):
         super().setUp()
-        self.browser.find_element_by_css_selector(
-            '[name="key"]').send_keys(self.passphrase)
-        self.browser.find_element_by_css_selector(
-            '[name="key_confirmation"]').send_keys(self.passphrase)
-        self.browser.find_element_by_css_selector(
-            '[type="submit"]').click()
+        self._setup_user()
+        self._create_report()
+
+    def _setup_user(self):
+        self.client.login(
+            username=self.username,
+            password=self.password,
+        )
+        cookie = self.client.cookies['sessionid']
+        self.browser.get(self.live_server_url)
+        # ref: https://github.com/ariya/phantomjs/issues/14228
+        try:
+            self.browser.add_cookie({
+                'name': 'sessionid',
+                'value': cookie.value,
+                'secure': False,
+                'path': '/',
+            })
+        except BaseException:
+            self.browser.add_cookie({
+                'name': 'sessionid',
+                'value': cookie.value,
+                'secure': False,
+                'path': '/',
+                'domain': 'localhost',
+            })
+        self.browser.get(self.live_server_url)
+
+    def _create_report(self):
+        self.element.enter_key()
+        self.element.enter_key_confirmation()
+        self.element.submit()
+
+
+class EncryptedFrontendTest(
+    FrontendTestCase,
+):
+    pass
