7a5ba58349dd857ba948488485ce66dbe75e1e41
Lynn Cyrin <firemagelynn@gmail.com>
2017-11-23 17:58:04 -0800

    back to headless

--- callisto_core/tests/delivery/test_frontend.py
+++ callisto_core/tests/delivery/test_frontend.py
@@ -70,9 +70,9 @@ class FrontendCase(
     @classmethod
     def setup_browser(cls):
         chrome_options = Options()
-        # chrome_options.add_argument("--headless")
-        # chrome_options.add_argument('--no-sandbox')
-        # chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument("--disable-gpu")
         cls.browser = webdriver.Chrome(
             chrome_options=chrome_options,
         )
