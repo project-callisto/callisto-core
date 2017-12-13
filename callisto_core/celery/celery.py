5a3e43d5c382896e0511fa5f5f9e75caf0af9bd0
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2017-12-13 17:36:01 +0000

    Cleaned up settings

--- callisto_core/celery/celery.py
+++ callisto_core/celery/celery.py
@@ -6,7 +6,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'utils.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'callisto_core.utils.settings')
 
 app = Celery('notification')
 
