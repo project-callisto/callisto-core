c5e32f952cc47355227ef0c8f926f31d02114596
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2017-12-19 19:32:29 +0000

    Updated settings

--- callisto_core/celery/celery.py
+++ callisto_core/celery/celery.py
@@ -21,7 +21,7 @@ configurations.setup()
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
