91b65358bb204f6dd96a560eb0756c9c114b14fb
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2017-12-19 18:11:09 +0000

    added back configs

--- callisto_core/celery/celery.py
+++ callisto_core/celery/celery.py
@@ -7,6 +7,9 @@ from celery import Celery
 
 app = Celery('celery')
 
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'callisto_core.utils.settings')
+
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
