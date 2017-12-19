ef666df019e989cd49f8ef9ee59aca6483d0bbf5
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2017-12-19 20:16:27 +0000

    Fixed celery init

--- callisto_core/celery/celery.py
+++ callisto_core/celery/celery.py
@@ -24,7 +24,7 @@ configurations.setup()
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks(settings.INSTALLED_APPS)
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
