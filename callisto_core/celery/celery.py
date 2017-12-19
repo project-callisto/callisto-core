1b5303d11b5a444c3fa3d977f9b6926f341eeb3e
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2017-12-19 17:28:03 +0000

    Removed settings override

--- callisto_core/celery/celery.py
+++ callisto_core/celery/celery.py
@@ -5,9 +5,6 @@ import os
 
 from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'callisto_core.utils.settings')
-
 app = Celery('notification')
 
 # Using a string here means the worker doesn't have to serialize
