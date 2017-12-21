cf3fa8489e36f52f214176d72b45e2cb6758e7be
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2017-12-21 08:10:00 +0000

    $ make clean

--- callisto_core/celeryconfig/tests/test_celery.py
+++ callisto_core/celeryconfig/tests/test_celery.py
@@ -1,15 +1,10 @@
-from pytest import raises
+from django.test import TestCase
 
-from celery.exceptions import Retry
 from callisto_core.celeryconfig.tasks import add
 
-# for python 2: use mock.patch from `pip install mock`.
-from unittest.mock import patch
-from unittest import TestCase
-from django.test import TestCase, override_settings
 
 class TestCelery(TestCase):
-    
+
     def test_add_task(self):
         add.delay(1, 2)
 
