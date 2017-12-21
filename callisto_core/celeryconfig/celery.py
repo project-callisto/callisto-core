4c5973ed01dec4b85188008947a25d10f2c59d37
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2017-12-21 03:59:41 +0000

    Moved celery app to celeryconfig

--- callisto_core/celeryconfig/celery.py
+++ callisto_core/celeryconfig/celery.py
@@ -9,7 +9,7 @@ from celery import Celery
 from django.conf import settings
 
 logger = logging.getLogger(__name__)
-app = Celery('celery')
+app = Celery('celeryconfig')
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'callisto_core.utils.settings')
