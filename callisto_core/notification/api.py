7b6c8320d5fb8412cbdc5e35c2f1f27f899ff7fd
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2017-12-22 02:30:54 +0000

    Moved hardcoded settings to config file, added notification api from campus client

--- callisto_core/notification/api.py
+++ callisto_core/notification/api.py
@@ -1,24 +1,39 @@
+import copy
 import logging
+import os
 import typing
 
 import gnupg
+import requests
+from config import env
+from reportlab.lib.enums import TA_CENTER
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.units import inch
+from reportlab.platypus import Image, PageBreak, Paragraph, Spacer
+from tenant_schemas.utils import get_tenant_model
 
 from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+from django.db import connection
 from django.template import Context, Template
+from django.template.loader import get_template
 from django.utils import timezone
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 from callisto_core.reporting.report_delivery import (
     PDFFullReport, PDFMatchReport,
 )
+from callisto_core.utils.tenant_api import CallistoCoreTenantApi
 
 logger = logging.getLogger(__name__)
 
 
 class CallistoCoreNotificationApi(object):
 
-    report_filename = "report_{0}.pdf.gpg"
-    report_title = 'Report'
+    report_filename = settings.NOTIFICATION_REPORT_FILENAME
+    report_title = settings.NOTIFICATION_REPORT_TITLE
 
     @property
     def from_email(self):
@@ -26,7 +41,7 @@ class CallistoCoreNotificationApi(object):
 
     @property
     def mail_domain(_):
-        return settings.APP_URL
+        return settings.NOTIFICATION_MAIL_DOMAIN
 
     @property
     def model(_):
@@ -293,3 +308,240 @@ class CallistoCoreNotificationApi(object):
             self.context['subject'],
             self.context['notification_name'],
         ))
+
+
+class CampusTenantApi(CallistoCoreTenantApi):
+
+    @staticmethod
+    def site_settings(*args, **kwargs):
+        return env.site_settings(*args, **kwargs)
+
+
+class CampusNotificationApi(CallistoCoreNotificationApi):
+
+    report_title = settings.NOTIFICATION_REPORT_TITLE
+    report_filename = settings.NOTIFICATION_REPORT_FILENAME
+    mail_domain = settings.NOTIFICATION_MAIL_DOMAIN
+
+    @property
+    def site_id(self):
+        return connection.tenant.site_id
+
+    @property
+    def from_email(self):
+        return f'"Callisto" <noreply@{self.mail_domain}>'
+
+    @property
+    def mailgun_post_route(self):
+        return f"https://api.mailgun.net/v3/{self.mail_domain}/messages"
+
+    def user_site_id(self, user):
+        return user.account.site_id
+
+    def get_cover_page(self, report_id, recipient):
+        title = f"{self.report_title} No.: {report_id}"
+
+        styles = getSampleStyleSheet()
+        headline_style = styles["Heading1"]
+        headline_style.alignment = TA_CENTER
+        headline_style.fontSize = 48
+        subtitle_style = styles["Heading2"]
+        subtitle_style.fontSize = 24
+        subtitle_style.leading = 26
+        subtitle_style.alignment = TA_CENTER
+
+        CoverPage = []
+        logo = os.path.join(
+            settings.BASE_DIR,
+            'callisto_site/static/images/callisto_logo.png',
+        )
+
+        image = Image(logo, 3 * inch, 3 * inch)
+        CoverPage.append(image)
+        CoverPage.append(Spacer(1, 18))
+        CoverPage.append(Paragraph("CONFIDENTIAL", headline_style))
+        CoverPage.append(Spacer(1, 30))
+        CoverPage.append(Spacer(1, 40))
+        CoverPage.append(Paragraph(title, subtitle_style))
+        CoverPage.append(Spacer(1, 40))
+        paragraph = Paragraph(
+            f"Intended for: {recipient}, Title IX Coordinator", subtitle_style)
+        CoverPage.append(paragraph)
+        CoverPage.append(PageBreak())
+        return CoverPage
+
+    # entrypoints
+
+    def send_with_kwargs(self, **kwargs):
+        # eventually the `send` method will work this way
+        self.context = {**kwargs}
+        self.send()
+
+    def send_student_verification_email(self, form, *args, **kwargs):
+        email = form.cleaned_data.get('email')
+        for user in form.get_users(email):
+            self.context = {
+                'to_addresses': [email],
+                'email_subject': 'Verify your student email',
+                'email_name': 'student_verification_email',
+                'redirect_url': form.redirect_url,
+                'email_template_name': form.view.email_template_name,
+                'user': form.view.request.user,
+            }
+            self._send_verification_email(user, *args, **kwargs)
+
+    def send_password_reset_email(self, form, *args, **kwargs):
+        email = form.cleaned_data.get('email')
+        for user in form.get_users(email):
+            self.context = {
+                'to_addresses': [email],
+                'email_subject': 'Reset your password',
+                'email_name': 'password_reset_email',
+                'uid': args[2]['uid'],
+                'protocol': args[2]['protocol'],
+                'email_template_name': args[1],
+                'user': args[2]['user'],
+                'token': args[2]['token'],
+            }
+            self._send_verification_email(user, *args, **kwargs)
+
+    def _send_verification_email(self, user, *args, **kwargs):
+        domain = self._get_account_domain(settings.APP_URL, user)
+
+        self.context.update({
+            'site_id': user.account.site_id,
+            'domain': domain,
+        })
+        self.send()
+
+    def _get_account_domain(self, domain, user):
+        Tenant = get_tenant_model()
+
+        # Set the domain URL, handle special case for staging:
+        if settings.DEBUG:
+            domain = Tenant.objects.get(
+                site_id=user.account.site_id).domain_url
+
+            domain_end = domain.split('.')[1] + '.' + domain.split('.')[2]
+            domain = '{front}-staging.{back}'.format(
+                front=domain.split('.')[0], back=domain_end)
+        else:
+            domain = Tenant.objects.get(
+                site_id=user.account.site_id).domain_url
+
+        return domain
+
+    def send_ucb_activation_email(self, user, email):
+        domain = self._get_account_domain(settings.APP_URL, user)
+
+        # TODO: mirror send_password_reset_email
+        self.context = {
+            'domain': domain,
+            'email_template_name': 'account_activation_email.html',
+            'to_addresses': [email],
+            'site_id': user.account.site_id,
+            'user': user,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': default_token_generator.make_token(copy.copy(user)),
+            'protocol': 'http' if settings.DEBUG else 'https',  # TODO: not this
+            'email_subject': 'Keep Our Community Safe with Callisto',
+            'email_name': 'ucb_activation_email',
+        }
+        self.send()
+
+    # send cycle
+
+    @property
+    def _mail_domain(self):
+        if (
+            settings.APP_URL in ['localhost', 'testserver'] or
+            settings.DEBUG
+        ):
+            return self.mail_domain
+        else:
+            Tenant = get_tenant_model()
+            domain = Tenant.objects.get(site_id=self.site_id).domain_url
+            partner_name = domain.split('.')[0]
+
+            return f'{partner_name}mail.callistocampus.org'
+
+    @property
+    def _extra_data(self):
+        # for tests!
+        return {}
+
+    @property
+    def _mail_attachments(self):
+        files = {'files': []}
+        if self.context.get('attachment'):
+            file_name = self.context['attachment'][0]
+            file_data = self.context['attachment'][1]
+            files['files'].append(
+                ('attachment', (file_name, file_data)),
+            )
+        return files
+
+    def set_notification(self):
+        if self.context.get('email_template_name'):
+            body = get_template(
+                self.context['email_template_name']).template.source
+            self.context.update({
+                'notification_name': self.context['email_template_name'],
+                'subject': self.context['email_subject'],
+                'body': body,
+            })
+        else:
+            return super().set_notification()
+
+    def send_email(self):
+        response = requests.post(
+            self.mailgun_post_route,
+            auth=("api", settings.MAILGUN_API_KEY),
+            data={
+                "from": self.context.get('from_email', self.from_email),
+                "to": self.context['to_addresses'],
+                "subject": self.context['subject'],
+                "html": self.context['body'],
+                **self._extra_data,
+            },
+            **self._mail_attachments,
+        )
+        self.context.update({
+            'response': getattr(response, 'context', response),
+            'response_status': response.status_code,
+            'response_content': response.content,
+        })
+
+    def log_action(self):
+        super().log_action()
+
+        if self.context.get('attachment'):
+            self.context.update({
+                'attachment': (
+                    self.context['attachment'][0],
+                    'FILEDATA',
+                )
+            })
+
+        if self.context.get('body'):
+            self.context.update({
+                'body': self.context['body'][:80]
+            })
+
+        if not self.context.get('response_status') == 200:
+            logger.error(f'status_code!=200, context: {self.context}')
+
+
+class LoggingNotificationApi(CampusNotificationApi):
+
+    @property
+    def _extra_data(self):
+        return {'o:testmode': 'yes'}
+
+    def log_action(self):
+        super().log_action()
+        for key, value in self.context.items():
+            self._logging(**{key: value})
+
+    def _logging(self, *args, **kwargs):
+        pass
