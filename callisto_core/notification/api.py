e382f44a5470ae9517d1ab71eaa9d3b3143d1aaa
Lynn Cyrin <firemagelynn@gmail.com>
2018-01-02 22:46:16 -0800

    logo path

--- callisto_core/notification/api.py
+++ callisto_core/notification/api.py
@@ -79,7 +79,7 @@ class CallistoCoreNotificationApi(object):
         CoverPage = []
         logo = os.path.join(
             settings.BASE_DIR,
-            'assets/callisto_logo.png',
+            '../../assets/callisto_logo.png',
         )
 
         image = Image(logo, 3 * inch, 3 * inch)
