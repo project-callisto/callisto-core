28063ff17814f19d6376a4a8443c2e3e32676ebe
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2018-01-29 17:50:59 +0000

    Moved tests

--- callisto_core/celeryconfig/tests/test_celery.py
+++ callisto_core/celeryconfig/tests/test_celery.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from callisto_core.celeryconfig.tasks import add, send_email
+from callisto_core.celeryconfig.tasks import add
 
 
 class TestCelery(TestCase):
@@ -11,17 +11,3 @@ class TestCelery(TestCase):
     def test_task_result(self):
         result = add.delay(1, 2)
         self.assertEqual(result.get(timeout=3), 3)
-
-
-class TestAsyncEmail(TestCase):
-
-    def test_send_email_basic_case(self):
-        send_email.delay(
-            email_data={
-                'to': 'test@example.com',
-                'subject': 'test',
-                'html': '<h1>test</h1>',
-                'o:testmode': 'yes',
-            },
-            email_attachments=None,
-        )
