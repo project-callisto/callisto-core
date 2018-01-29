2e2270aac3a8c8e3bdac37b21c4c8141cb947edc
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2018-01-29 19:46:47 +0000

    Email test

---
+++ callisto_core/tests/notification/test_email.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+from callisto_core.notification import tasks
+
+
+class TestAsyncEmail(TestCase):
+
+    def test_send_email_basic_case(self):
+        tasks.SendEmail.delay(
+            email_data={
+                'to': 'test@example.com',
+                'subject': 'test',
+                'html': '<h1>test</h1>',
+                'o:testmode': 'yes',
+            },
+            email_attachments=None,
+        )
