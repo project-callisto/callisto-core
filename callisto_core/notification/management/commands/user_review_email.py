a976df6f83e96a7c50997b1debc3ac4cabc239d0
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-06 10:41:52 -0700

    send_user_review_email management command shell

---
+++ callisto_core/notification/management/commands/user_review_email.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def send_user_review_email(self):
+        pass
+
+    def handle(self, *args, **options):
+        self.send_user_review_email()
