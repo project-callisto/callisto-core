ac72bdacfe29084f3196adb77c43564213e08c7a
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-09 16:06:51 -0700

    fill out test_demo_mode_calls test

--- callisto_core/tests/reporting/test_demo_mode.py
+++ callisto_core/tests/reporting/test_demo_mode.py
@@ -1,9 +1,11 @@
 from unittest.mock import call, patch
 
+from callisto_core.utils.sites import TempSiteID
 from callisto_core.tests.utils.api import CustomNotificationApi
 from callisto_core.tests.test_base import ReportFlowHelper as ReportFlowTestCase
-
 from callisto_core.reporting.views import ReportingConfirmationView
+
+from django.contrib.sites.models import Site
 
 class ConfirmationViewTest(
     ReportFlowTestCase,
@@ -17,7 +19,9 @@ class ConfirmationViewTest(
         self.assertEqual(api_logging.call_count, 3)
 
     def test_demo_mode_calls(self):
-        with patch.object(CustomNotificationApi, 'send_email') as api_logging:
+        Site.objects.get_or_create(id=4)
+
+        with TempSiteID(4), patch.object(CustomNotificationApi, 'send_email') as api_logging:
             self.client_post_report_creation()
             self.client_post_reporting_end_step()
 
