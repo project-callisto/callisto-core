8df209453e306c259423556f09061701ba977b2b
Ari Adair <ari@projectcallisto.org>
2019-06-07 23:45:57 +0000

    import

--- callisto_core/accounts/auth.py
+++ callisto_core/accounts/auth.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth.models import User
 
+import bcrypt
+
 from callisto_core.accounts.models import Account
 
 
